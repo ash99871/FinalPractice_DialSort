b4c9960ec7a2e0c45db69726e4d999ec535767c9
ash99871 <asrobayop@eafit.edu.co>
2026-05-02 17:20:39 -0500

    Radix Sort and Dial Sort implementation

--- main.cpp
+++ main.cpp
@@ -2,71 +2,43 @@
 #include <vector>
 #include <chrono>
 #include "dialSort.cpp"
-#include "radixSort.cpp"
 
 using namespace std;
 
-int main()
-{
-    // numero/tamaño del dataset
+int main() {
     vector<size_t> Ns = {100000};
-    // universo de valores
     vector<int> Us = {1000};
 
     for (auto n : Ns) {
         for (auto U : Us) {
-
             cout << "\n========================\n";
             cout << "N = " << n << " | U = " << U << endl;
 
-            // generar dataset
-            auto datos = gen_uniform(n, U, 123);
+            
+            const auto datos = gen_uniform(n, U, 123);
 
-            // mostrar 20 desordenados
-            cout << "\nPrimeros 20 datos DESORDENADOS:\n";
-            for (int i = 0; i < 20; i++) {
-                cout << datos[i] << " ";
-            }
-            cout << endl;
-
-            // dialSort
+            // --- PRUEBA DIALSORT ---
+            auto datos_dial = datos; 
+            
             auto start = chrono::high_resolution_clock::now();
-            dialsort(datos);
+            dialsort(datos_dial);
             auto end = chrono::high_resolution_clock::now();
-
+            
             double time_dial = chrono::duration<double, milli>(end - start).count();
+            cout << "DialSort Tiempo: " << time_dial << " ms" << endl;
 
-            // Mostrar 50 ordenados
-            cout << "\nPrimeros 50 datos ORDENADOS:\n";
-            for (int i = 0; i < 300; i++) {
-                cout << datos[i] << " ";
-            }
-            cout << endl;
-
-            // tiempo
-            cout << "\nDialSort Tiempo: " << time_dial << " ms" << endl;
-
-            //desordenar
-            datos = gen_uniform(n, U, 123);
-
-            //radix sort
+            // --- PRUEBA RADIXSORT ---
+            auto datos_radix = datos; 
+            
             auto one = chrono::high_resolution_clock::now();
-            radixSort(datos);
+            radixSort(datos_radix);
             auto two = chrono::high_resolution_clock::now();
-
-            double time_radix=chrono::duration<double, milli>(two - one).count();
-
-            // Mostrar 50 ordenados
-            cout << "\nPrimeros 50 datos ORDENADOS:\n";
-            for (int i = 0; i < 300; i++) {
-                cout << datos[i] << " ";
-            }
-            cout << endl;
-
-            cout << "\nRadixSort Tiempo: " << time_radix << " ms" << endl;
+            
+            double time_radix = chrono::duration<double, milli>(two - one).count();
+            cout << "RadixSort Tiempo: " << time_radix << " ms" << endl;
+            
             
         }
     }
-
     return 0;
 }
