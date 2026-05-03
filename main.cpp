cc6d38d76b8be19648be08220be8798e00d23ea8
ash99871 <asrobayop@eafit.edu.co>
2026-05-03 18:34:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <chrono>
-#include "dialSort.h"
-#include "radixSort.h"
+#include "benchmark.h"
 
 using namespace std;
 
+
 int main() {
-    vector<size_t> Ns = {100000};
-    vector<int> Us = {1000};
+    
+    Benchmark Benchmark;
 
-    for (auto n : Ns) {
-        for (auto U : Us) {
-            cout << "\n========================\n";
-            cout << "N = " << n << " | U = " << U << endl;
+    cout << "Iniciando prueba." << endl;
+    cout << "Configuracion: n = 1,000,000 elementos, U = 1,000 de rango." << endl;
 
-            
-            const auto datos = gen_uniform(n, U, 123);
+    
+    Benchmark.run();
 
-            // --- PRUEBA DIALSORT ---
-            auto datos_dial = datos; 
-            
-            auto start = chrono::high_resolution_clock::now();
-            dialsort(datos_dial);
-            auto end = chrono::high_resolution_clock::now();
-            
-            double time_dial = chrono::duration<double, milli>(end - start).count();
-            cout << "DialSort Tiempo: " << time_dial << " ms" << endl;
+    cout << "\nPrueba finalizada con exito." << endl;
 
-            // --- PRUEBA RADIXSORT ---
-            auto datos_radix = datos; 
-            
-            auto one = chrono::high_resolution_clock::now();
-            radixSort(datos_radix);
-            auto two = chrono::high_resolution_clock::now();
-            
-            double time_radix = chrono::duration<double, milli>(two - one).count();
-            cout << "RadixSort Tiempo: " << time_radix << " ms" << endl;
-            
-            
-        }
-    }
     return 0;
 }
+
