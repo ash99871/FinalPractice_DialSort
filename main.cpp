474631c046d46bb0d054c773e012e032081074b7
ash99871 <asrobayop@eafit.edu.co>
2026-05-09 13:02:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,8 @@ int main() {
     cout << "Iniciando prueba." << endl;
     cout << "Configuracion: n = 1,000,000 elementos, U = 1,000 de rango." << endl;
 
-    Benchmark benchmark;
     
-    benchmark.run();
+    Benchmark::run();
 
     cout << "\nPrueba finalizada con exito." << endl;
 
